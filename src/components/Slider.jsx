84be290de1fa15ea34f7d73b2cc6c87152684e21
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-09-29 17:19:20 +0900

    created slide show in carousel

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,14 @@
 import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import { useState } from "react";
 import styled from "styled-components";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -23,15 +25,83 @@ const Arrow = styled.div`
   left: ${(props) => props.direction === "left" && "10px"};
   right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+  transition: all 1.5s ease;
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <KeyboardArrowLeft />
       </Arrow>
-      <Arrow direction="right">
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOP NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <KeyboardArrowRight />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "IMG_2926.JPG",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE!",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "IMG_2926.JPG",
+    title: "AUTUMN SALE",
+    desc: "DON'T COMPROMISE ON STYLE!",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "IMG_2926.JPG",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE!",
+    bg: "fbf0f4",
+  },
+];
