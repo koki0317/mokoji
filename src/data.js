71a4a2ce7587901ea5d3149d84130a0b2494af38
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-09-30 15:06:10 +0900

    created category and categoryitems

--- src/data.js
+++ src/data.js
@@ -21,3 +21,56 @@ export const sliderItems = [
     bg: "fbf0f4",
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: "IMG_2926.JPG",
+    title: "Mokoji1",
+  },
+  {
+    id: 2,
+    img: "IMG_2926.JPG",
+    title: "Mokoji2",
+  },
+  {
+    id: 2,
+    img: "IMG_2926.JPG",
+    title: "Mokoji3",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "IMG_2926.JPG",
+  },
+  {
+    id: 2,
+    img: "IMG_2926.JPG",
+  },
+  {
+    id: 3,
+    img: "IMG_2926.JPG",
+  },
+  {
+    id: 4,
+    img: "IMG_2926.JPG",
+  },
+  {
+    id: 5,
+    img: "IMG_2926.JPG",
+  },
+  {
+    id: 6,
+    img: "IMG_2926.JPG",
+  },
+  {
+    id: 7,
+    img: "IMG_2926.JPG",
+  },
+  {
+    id: 8,
+    img: "IMG_2926.JPG",
+  },
+];
